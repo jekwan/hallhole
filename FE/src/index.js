4bbba167cf07709c7f37e8b212c48088a9fe5bb5
jekwan <jekwan@kakao.com>
2022-08-19 02:04:33 +0900

    [Fix] index.js 버그 수정

--- FE/src/index.js
+++ FE/src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+
 import App from "./components/App";
 
 import Theme from "./configs/theme";
