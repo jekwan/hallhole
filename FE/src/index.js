8fa205ca16df2c2b06b733c2a3efa191152d64f5
jekwan <jekwan@kakao.com>
2022-08-19 02:39:56 +0900

    [Fix] master 복원

---
+++ FE/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+
+import App from "./components/App";
+
+import Theme from "./configs/theme";
+import Store from "./configs/store";
+
+const rootElement = document.getElementById("app");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ThemeProvider theme={Theme}>
+      <BrowserRouter>
+        <Provider store={Store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>,
+);
---
+++ FE/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /\.module\.css$/i, // 모듈 파일 제외 설정
+        use: ["style-loader", "css-loader"],
+      },
+      // CSS Module ([filename].module.css)
+      {
+        test: /\.module\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: {
+      index: "/index.html",
+    },
+    static: {
+      directory: path.join(__dirname, "static"),
+    },
+    proxy: {
+      "/ws": {
+        target: "https://i7a401.p.ssafy.io",
+        changeOrigin: true,
+      },
+    },
+  },
+};
